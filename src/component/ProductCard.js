513719061357fec240ef3c01bf400a02e3364854
sapiens12 <sapiens12@nate.com>
2024-03-27 23:01:54 +0100

    build 오류 수정

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -7,7 +7,7 @@ const ProductCard = ({ item }) => {
         <img src={item?.img} alt='상품이미지' />
         <div
           className={`label_conscious ${
-            item?.choice == true ? "" : "none_conscious"
+            item?.choice === true ? "" : "none_conscious"
           }`}
         >
           THE CONSCIOUS CHOICE
@@ -16,7 +16,7 @@ const ProductCard = ({ item }) => {
       <div className='item_details'>
         <div className='item_heading'>{item?.title}</div>
         <div className='item_price'>₩{item?.price.toLocaleString("ko-KR")}</div>
-        <div className='new_product'>{item?.new == true ? "신제품" : ""}</div>
+        <div className='new_product'>{item?.new === true ? "신제품" : ""}</div>
       </div>
     </div>
   );
