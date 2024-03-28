f0499391b116ce72614a37d14b8971fbd995d0ed
sapiens12 <sapiens12@nate.com>
2024-03-28 18:48:15 +0100

    my json 추가

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,7 +5,8 @@ import ProductCard from "../component/ProductCard";
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    let url = "http://localhost:5000/products";
+    let url =
+      "https://my-json-server.typicode.com/<KIMMIHYE12>/<hnm-react-router-practice>/products";
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
