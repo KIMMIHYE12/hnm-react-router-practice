ff8da1bdd96e9afb91ae187cb391e6bb08758251
sapiens12 <sapiens12@nate.com>
2024-03-29 19:39:18 +0100

    로그인 반응형 수정

--- src/page/Login.js
+++ src/page/Login.js
@@ -19,10 +19,10 @@ const Login = ({ setAuthenticate }) => {
           className='mb-3 login_field'
           controlId='formPlaintextEmail'
         >
-          <Form.Label column sm='1'>
+          <Form.Label column lg='1' md='2' sm='2'>
             이메일
           </Form.Label>
-          <Col sm='3'>
+          <Col md='4' sm='6'>
             <Form.Control type='email' placeholder='name@example.com' />
           </Col>
         </Form.Group>
@@ -32,15 +32,15 @@ const Login = ({ setAuthenticate }) => {
           className='mb-3 login_field'
           controlId='formPlaintextPassword'
         >
-          <Form.Label column sm='1'>
+          <Form.Label column lg='1' md='2' sm='2'>
             비밀번호
           </Form.Label>
-          <Col sm='3'>
+          <Col md='4' sm='6'>
             <Form.Control type='password' placeholder='Password' />
           </Col>
         </Form.Group>
         <Row className='btn_login_content'>
-          <Col sm='4'>
+          <Col lg='5' md='6' sm='8'>
             <button type='submit'>로그인</button>
           </Col>
         </Row>
